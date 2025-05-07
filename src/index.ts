535cc1152ff1b9ee55f04f5f2d9d72cfb6580503
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-05-07 22:29:57 +0600

    function filterByRating has been created

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,17 @@ function formatString(input: string, toUpper?: boolean): string {
 console.log(formatString("Hello"))  
 console.log(formatString("Hello", true))  
 console.log(formatString("Hello", false))  
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+  const filteredItems = items.filter(item =>item.rating >= 4);
+  return filteredItems;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+console.log(filterByRating(books))
